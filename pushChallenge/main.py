f56c6b954a226cacf315f48fcc0def64e678da12
Sigurdur Haukur Birgisson <sigurdur.h.b@icloud.com>
2020-09-15 15:17:04 +0000

    lesson1

--- pushChallenge/main.py
+++ pushChallenge/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port
+from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
 ev3 = EV3Brick()
@@ -14,15 +14,17 @@ right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Go forward and backwards for one meter.
-robot.straight(1000)
-ev3.speaker.beep()
+print("Push Challenge")
 
-robot.straight(-1000)
-ev3.speaker.beep()
+def y_axis(millimeters=0, speed=100):
+    Drive_speed = speed
+    print("forward {millimeters}")
+    robot.straight(millimeters)
+    ev3.speaker.beep()
 
-# Turn clockwise by 360 degrees and back again.
-robot.turn(360)
-ev3.speaker.beep()
+def x_axis(degrees=0):
+    print("turning {degrees}degrees")
+    robot.turn(degrees)
 
-robot.turn(-360)
+# y_axis(500, 200)
 ev3.speaker.beep()
